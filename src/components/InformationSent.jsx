ac3ca1fba268fc74c6ac363042cb63a150bf5460
Alonsop <alonsop@uakami.com>
2017-11-03 16:04:15 -0400

    fix(InformationSent): Fixing Component and make changes according to styleguide

--- src/components/InformationSent.jsx
+++ src/components/InformationSent.jsx
@@ -1,55 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Media } from 'reactstrap'
 
+import logo from '../images/LOGO_Aracar-01.svg'
+import checkmark from '../images/request sent_icon-01.svg'
 
-const InformationSent = () => {
-    return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-6 left-column" align="center">
-                    <div>
-                        <h1 className="mt-5 left-title">Information sent</h1>
+
+class  InformationSent extends Component {
+
+    render () {
+
+        return (
+            <div className="container mt-5 mb-5 pt-lg-5 pb-lg-5">
+                <div className="row">
+                    <div className="col-6 left-column" align="center">
+                        <br/>
+                        <br/>
+                        <br/>
+                        <div className="mt-5">
+                            <h1 className="mt-5 left-subtitle">Information sent</h1>
+                        </div>
+
                     </div>
 
-                </div>
 
+                    <div className="col-6 right-column " align="center">
 
-                <div className="col-6 right-column " align="center">
-
-                    <div class="text-center img-aracar mt-4">
-                        <img src="https://snag.gy/r7zhqW.jpg" class="rounded mx-auto d-block" alt="..."/>
-                    </div>
+                        <div className="text-center img-aracar mt-5">
+                            <img src={logo} className="rounded mx-auto d-block" alt="..."/>
+                        </div>
 
 
 
-                    <div class="container">
-                        <div class="row align-items-center">
-                            <div class="col mt-lg-5 mt-md-5 mt-sm-5 mt-xl-5">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col mt-lg-5 mt-md-5 mt-sm-5 mt-xl-5">
 
-                                <div>
-                                    <img src="https://snag.gy/xpzLnV.jpg" alt=""/>
-                                </div>
+                                    <div className="mt-5 mb-5">
+                                        <img src={checkmark} alt=""/>
+                                    </div>
 
-                                <div>
-                                    <h1>Thank you for applying</h1>
-                                </div>
+                                    <div>
+                                        <h2 className="right-title-thank-you">Thank you for applying</h2>
+                                    </div>
 
-                                <div className="mt-lg-4 mt-md-4 mt-sm-4 mt-xl-4 mb-lg-5 mb-md-5 mb-sm-5 mb-xl-5">
-                                    <h7>We will contact</h7>
-                                </div>
+                                    <div className="mt-lg-4 mt-md-4 mt-sm-4 mt-xl-4 mb-lg-5 mb-md-5 mb-sm-5 mb-xl-5">
+                                        <h6 className="will-contact">We will contact</h6>
+                                    </div>
 
-                                <div className="mt-lg-5 mt-md-5 mt-sm-5 mt-xl-5 mb-lg-5 mb-md-5 mb-sm-5 mb-xl-5">
-                                    <Button color="primary" onClick="#">Back to the site</Button>
+                                    <div className="mt-lg-5 mt-md-5 mt-sm-5 mt-xl-5 mb-lg-5 mb-md-5 mb-sm-5 mb-xl-5 mb-5">
+                                        <Button color="primary" onClick="#">Back to the site</Button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 export default InformationSent
